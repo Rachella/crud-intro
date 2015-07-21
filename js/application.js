324819e71c7fea3475a492eacfd8537d85926a11
Rachel <rachella@gmail.comOB>
2015-07-21 22:09:04 +0800

    POST request created

--- js/application.js
+++ js/application.js
@@ -1,6 +1,9 @@
 $(function () {
 
   var $entries = $('.entries');
+  var $user = $('#user');
+  var $title = $('#title');
+  var $text = $('#text');
 
   $.ajax({
     type: 'GET',
@@ -10,11 +13,33 @@ $(function () {
       $.each(entries, function(i, entry) {
         $entries.append('<li> id: ' + entry._id + ' title: ' + entry.title + 'text: ' + entry.text + ' user: ' + entry.user + '</li>');
       });
-      console.log(entries);
+      console.log('Success', entries);
     },
     error: function() {
-      console.log('error');
+      console.log('Error getting posts');
     }
+  });
+
+  $('#add-post').on('click', function() {
+
+    $.ajax({
+      type: 'POST',
+      url: 'http://ga-wdi-api.meteor.com/api/posts/',
+      data: {      
+        user: $user.val(),
+        title: $title.val(),
+        text: $text.val()
+      },
+      dataType: 'json',
+      success: function(newEntry) {
+        $entries.append('<li> id: ' + newEntry._id + ' title: ' + newEntry.title + 'text: ' + newEntry.text + ' user: ' + newEntry.user + '</li>');
+        console.log('Success in posting');
+      },
+      error: function() {
+        console.log('Error adding post');
+      }
+    });
+
   });
 
 })
